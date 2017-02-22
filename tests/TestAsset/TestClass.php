eb0059cf19a94bf6daf56df35e7cbca80871b6ba
Nick Sagona <nick@nolainteractive.com>
2017-02-22 10:39:43 -0600

    Update Copyright

--- tests/TestAsset/TestClass.php
+++ tests/TestAsset/TestClass.php
@@ -11,7 +11,7 @@ use Pop\Filter\ConvertCase as CC;
  * @category   Pop
  * @package    Pop_Test
  * @author     Nick Sagona, III <dev@nolainteractive.com>
- * @copyright  Copyright (c) 2009-2016 NOLA Interactive, LLC. (http://www.nolainteractive.com)
+ * @copyright  Copyright (c) 2009-2017 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
  * @version    2.0.0
  */
--- tests/TestAsset/TestParent.php
+++ tests/TestAsset/TestParent.php
@@ -8,7 +8,7 @@ namespace Pop\Code\Test\TestAsset;
  * @category   Pop
  * @package    Pop_Test
  * @author     Nick Sagona, III <dev@nolainteractive.com>
- * @copyright  Copyright (c) 2009-2016 NOLA Interactive, LLC. (http://www.nolainteractive.com)
+ * @copyright  Copyright (c) 2009-2017 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
  * @version    2.0.0
  */
